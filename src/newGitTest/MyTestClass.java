397640fcd69d841a27f4feb95815158059031a7e
Jerome <jerome.marfleet@mail.mcgill.ca>
2016-01-10 14:28:01 -0500

    added missing semi-colon onto second println statement

--- src/newGitTest/MyTestClass.java
+++ src/newGitTest/MyTestClass.java
@@ -5,7 +5,7 @@ public class MyTestClass {
 	public static void main(String[] args) {
 
 		System.out.println("Hello, World!");
-		System.out.println("This is another line.")
+		System.out.println("This is another line.");
 	}
 
 }
