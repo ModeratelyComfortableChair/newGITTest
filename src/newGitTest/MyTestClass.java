5a7106d77a4979adb369ebaf2129d9d9308bb0b2
ModeratelyComfortableChair <jerome.marfleet@mail.mcgill.ca>
2016-01-10 14:23:24 -0500

    Update MyTestClass.java
    Changed additional print command to println
    Added another println command that prints "This is another line."

--- src/newGitTest/MyTestClass.java
+++ src/newGitTest/MyTestClass.java
@@ -4,7 +4,8 @@ public class MyTestClass {
 
 	public static void main(String[] args) {
 
-		System.out.print("Hello, World!");
+		System.out.println("Hello, World!");
+		System.out.println("This is another line.")
 	}
 
 }
