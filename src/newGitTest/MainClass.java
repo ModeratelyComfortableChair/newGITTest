355456fe6123385c04b6eb9f280e0b7da616609f
Jerome <jerome.marfleet@mail.mcgill.ca>
2016-01-16 15:47:53 -0500

    Made Classes MainClass and Print Thread to newGitTest Package. Classes are very bare. Just Pushing them as they are now, and will fill them later.

---
+++ src/newGitTest/MainClass.java
@@ -0,0 +1,13 @@
+package newGitTest;
+import java.util.*;
+import java.io.*;
+
+public class MainClass {
+
+	public static int threadCount;
+	public static int stop;
+	public static void main(String[] args){
+		
+	}
+	
+}
---
+++ src/newGitTest/PrintThread.java
@@ -0,0 +1,10 @@
+package newGitTest;
+
+public class PrintThread {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
